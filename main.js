6cedefea0057b0133cd37a3db26c3223493776cc
Miles Florence <milesflo15@gmail.com>
2015-11-04 12:54:24 -0800

    taking a break

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 console.log("sanity check!");
+
+var firstParagraph = document.getElementById('main');
+console.log(firstParagraph);
+
+var getAllElements = document.getElementsByTagName('ul');
+console.log(getAllElements)
+
+var getSingleElement = document.getElementsByTagName('ul')[0];
+console.log(getSingleElement) 
+
+allListItems = document.getElementsByTagName('li');
+console.log(allListItems);
+
+var evenElements = document.getElementsByClassName('even');
+console.log(evenElements)
+
+var oddElements = document.getElementsByClassName('odd');
+console.log(oddElements)
+
+var firstEvenElement = document.getElementsByClassName('even')[0];
+console.log(firstEvenElement)
